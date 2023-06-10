ab1866581777d5f990ba6238c47d47eda37af00b
Akash <akashriaz05@gmail.com>
2023-06-10 23:25:23 +0600

    instructor section is done

--- src/Layout/DashBoard.jsx
+++ src/Layout/DashBoard.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import {Outlet,Link} from 'react-router-dom'
 import useAdmin from "../hooks/useAdmin";
+import useInstructors from "../hooks/useInstructor";
 
 const DashBoard = () => {
   // const isAdmin = true;
   const [isAdmin] = useAdmin()
-  const isInstructors = false;
+  // const isInstructors = true;
+  const [isInstructors] = useInstructors()
   return (
     <div className="drawer lg:drawer-open">
       <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
