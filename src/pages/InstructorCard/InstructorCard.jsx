78e3e13e1376785a04028dd948d4dce9076aff24
Akash <akashriaz05@gmail.com>
2023-06-13 17:56:49 +0600

    project done

--- src/pages/InstructorCard/InstructorCard.jsx
+++ src/pages/InstructorCard/InstructorCard.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const InstructorCard = ({instructor}) => {
     const {name, photo,email} = instructor;
   return (
     <div className="mt-20">
-      <div className="card w-96 h-[400px] bg-base-100 shadow-xl">
+      <motion.div animate={{
+      rotate: [0, 0, 270, 270, 0],
+      borderRadius: ["20%", "20%", "50%", "50%", "20%"],
+      transition:{duration:10}
+    }}  className="card w-96 h-[400px] bg-base-100 shadow-xl">
         <figure className="px-10 pt-10 h-48">
           <img
             src={photo}
@@ -20,7 +25,7 @@ const InstructorCard = ({instructor}) => {
             <button className="btn btn-primary">View Details</button>
           </div>
         </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
